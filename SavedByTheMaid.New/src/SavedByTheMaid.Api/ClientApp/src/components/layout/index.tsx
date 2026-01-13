06e1ead45663262d859b455424d2a2ca855307c2
Eden berlus <eberlus@bravo.com.do>
2026-01-13 10:12:55 -0400

    fix

--- SavedByTheMaid.New/src/SavedByTheMaid.Api/ClientApp/src/components/layout/index.tsx
+++ SavedByTheMaid.New/src/SavedByTheMaid.Api/ClientApp/src/components/layout/index.tsx
@@ -27,7 +27,7 @@ export function Header() {
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
-            <img src="/logo-ecomaid.svg" alt="ecoMaid" className="h-10" />
+            <img src="/logo-ecomaid.svg" alt="ecoMaid" className="h-12 w-auto" />
           </Link>
 
           {/* Navigation */}
