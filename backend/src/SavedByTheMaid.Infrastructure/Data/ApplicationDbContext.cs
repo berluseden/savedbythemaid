5a7cf0e84f49eb4596b0cd31f13ae0f7843c45c2
Eden berlus <eberlus@bravo.com.do>
2026-04-18 17:20:03 -0400

    Implement IApplicationDbContext interface in ApplicationDbContext class

--- backend/src/SavedByTheMaid.Infrastructure/Data/ApplicationDbContext.cs
+++ backend/src/SavedByTheMaid.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SavedByTheMaid.Application.Interfaces;
 using SavedByTheMaid.Domain.Entities;
 
 namespace SavedByTheMaid.Infrastructure.Data;
 
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
